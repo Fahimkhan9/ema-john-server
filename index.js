1092848c5ba1fb69f8ea5a829f97b9036bac67d7
Fahimkhan9 <61534271+Fahimkhan9@users.noreply.github.com>
2020-10-01 15:26:28 +0600

    app-deployed-to-heroku

--- index.js
+++ index.js
@@ -7,11 +7,16 @@ const app = express()
 const port = 5000
 require('dotenv').config()
 
+
 app.use(cors())
 app.use(bodyparser.json())
 console.log(process.env.DB_PASS);
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vigvf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
+ 
+app.get("/",(req,res) => {
+  res.send("hello it is working")
+})
 
 const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology: true });
 client.connect(err => {
